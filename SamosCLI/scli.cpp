6bcf7de58c8f3191066bb1d04bf0d6c7d2d253c3
Arif Ahmed <arixx74@gmail.com>
2022-06-11 08:46:19 +0530

    add CLI help

--- SamosCLI/scli.cpp
+++ SamosCLI/scli.cpp
@@ -117,6 +117,37 @@ CmdType getCmd(string &line, char cmdArgs[MAX_CLI_INPUT_SZ]) {
   return INVALID;
 }
 
+void op_print_help_menu() {
+  int numEmptyChars = 80;
+  char emptyChar = '-';
+  cout << "commands:" << endl
+       << setfill(emptyChar) << setw(numEmptyChars) << left
+       << "    >add [Data,ID,Amount,Transaction Type,Date & Time,Tags] "
+       << " enter an entry into the current database" << endl
+
+       << setfill(emptyChar) << setw(numEmptyChars) << left << "    >load "
+       << " load from default file .saved_entries.csv" << endl
+
+       << setfill(emptyChar) << setw(numEmptyChars) << left << "    >load file_name "
+       << " load from file_name" << endl
+
+       << setfill(emptyChar) << setw(numEmptyChars) << left << "    >save "
+       << " save to default file .saved_entries.csv" << endl
+
+       << setfill(emptyChar) << setw(numEmptyChars) << left << "    >save file_name "
+       << " save to file_name" << endl
+
+       << setfill(emptyChar) << setw(numEmptyChars) << left << "    >get num entries "
+       << " display the number of entries available" << endl
+
+       << setfill(emptyChar) << setw(numEmptyChars) << left
+       << "    >get total debit OR >get total credit OR >get total amount "
+       << " display current total debit or credit or amount respectively" << endl
+
+       << setfill(emptyChar) << setw(numEmptyChars) << left << "    >get last entry "
+       << " display the last entered entry" << endl;
+}
+
 void op_add_entry(Core &core, char *cmdArgs) {
   EntryModel entry(cmdArgs);
   core.addEntry(entry);
@@ -228,6 +259,9 @@ int cliProc() {
         break;
       case GET:
         op_get(core, cmdArgs);
+        break;
+      case HELP:
+        op_print_help_menu();
         break;
       case INVALID:
         cout << "\"" << inputLine << "\" is not a valid command" << endl;
